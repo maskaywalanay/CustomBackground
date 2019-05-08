56e357a71850d9c5275440c2930f6dd51653c153
mwalanay@gmail.com <mwalanay@gmail.com>
2019-05-08 21:41:22 +0700

    custom3

--- CustomBackground/app/src/main/java/illegaller/aida/dev/art/cusback/BackgroundImg.java
+++ CustomBackground/app/src/main/java/illegaller/aida/dev/art/cusback/BackgroundImg.java
@@ -1,14 +1,68 @@
 package illegaller.aida.dev.art.cusback;
-import android.widget.*;
 import android.content.*;
+import android.os.*;
+import android.provider.*;
 import android.util.*;
+import android.widget.*;
 
 public class BackgroundImg extends ImageView
 {
+    
+    private Handler mHandler;
+    private Runnable mTricker;
+
+    private int settings;
+    
     public BackgroundImg(Context context, AttributeSet attr)
     {
         super(context, attr);
+        mHandler = new Handler();
+        mTricker = new Runnable()
+        {
+            public void run()
+            {
+                UpdateBackground();
+                mHandler.postDelayed(this,500);
+            }
+
+        };
+
+        mTricker.run();
     }
-    
-    
-}
+        
+    private void UpdateBackground() 
+    {
+        // permission setting //
+        settings = Settings.System.getInt(getContext().getContentResolver(), "settings_bg",0);
+        switch (settings){
+            default:{return;}
+            case 0:
+                {
+                    BackgroundImg.this.setBackgroundColor(0x00000000);
+                    return;
+                }
+
+            case 1:{
+                    setBackgroundResource(setResource("bg_1", "drawable"));
+                    return;
+                }
+
+            case 2:{
+                    setBackgroundResource(setResource("bg_2", "drawable"));
+                    return;
+                }
+
+            case 3:{
+                    setBackgroundResource(setResource("bg_3", "drawable"));
+                    return;
+                }
+            }
+        }
+        
+    public int setResource(String nama, String tipe)
+    {
+        return getContext().
+            getResources().getIdentifier(nama, tipe, getContext().
+                                         getPackageName());
+    }
+ }
--- CustomBackground/app/src/main/java/illegaller/aida/dev/art/cusback/ControlList.java
+++ CustomBackground/app/src/main/java/illegaller/aida/dev/art/cusback/ControlList.java
@@ -1,13 +1,36 @@
 package illegaller.aida.dev.art.cusback;
-import android.preference.*;
 import android.os.*;
+import android.preference.*;
+import android.provider.*;
 
-public class ControlList extends PreferenceActivity
+public class ControlList extends PreferenceActivity implements Preference.OnPreferenceChangeListener
 {
+    private ListPreference settings;
+    
     public void onCreate(Bundle bundle)
     {
         super.onCreate(bundle);
         addPreferencesFromResource(setResource("preferencess",  "xml"));
+        
+        settings = (ListPreference)findPreference("settings_bg");
+        long tik = Settings.System.getInt(getContentResolver(),"settings_bg", 500);
+        settings.setValue(String.valueOf(tik));
+        settings.setSummary(settings.getEntry());
+        settings.setOnPreferenceChangeListener((Preference.OnPreferenceChangeListener)this);
+
+    }
+        
+    public boolean onPreferenceChange(Preference preference , Object object)
+    {
+        if(preference == settings)
+        {
+            long tik = Long.valueOf(String.valueOf(object));
+            int s = settings.findIndexOfValue((String)object);
+            Settings.System.putLong(getContentResolver(),"settings_bg", (tik));
+            settings.setSummary(settings.getEntries()[s]);
+        }
+
+        return true;
     }
     
     public int setResource(String nama, String tipe)
